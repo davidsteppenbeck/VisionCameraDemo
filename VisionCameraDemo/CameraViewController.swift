f408f846252d8169d2be5d812a394c2ed0325db9
David Steppenbeck <steppenbeck@gmail.com>
2020-04-10 23:14:14 +0100

    Renamed main view controller to `CameraViewController`.

---
+++ VisionCameraDemo/CameraViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.swift
+//  VisionCameraDemo
+//
+//  Created by David Steppenbeck on 2020/04/10.
+//  Copyright © 2020 David Steppenbeck. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+}
