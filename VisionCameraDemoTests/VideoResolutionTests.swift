c9ec5f017bebcb65d1c7bb7b2ad625abee432387
David Steppenbeck <steppenbeck@gmail.com>
2020-05-08 15:20:14 +0100

    Added `VideoResolution` tests.

---
+++ VisionCameraDemoTests/VideoResolutionTests.swift
@@ -0,0 +1,27 @@
+//
+//  VideoResolutionTests.swift
+//  VisionCameraDemoTests
+//
+//  Created by David Steppenbeck on 2020/05/08.
+//  Copyright © 2020 David Steppenbeck. All rights reserved.
+//
+
+import XCTest
+import AVFoundation
+@testable import VisionCameraDemo
+
+final class VideoResolutionTests: XCTestCase {
+
+    func testPreset() {
+        XCTAssertEqual(VideoResolution.photo.preset, AVCaptureSession.Preset.photo)
+        XCTAssertEqual(VideoResolution.high.preset, AVCaptureSession.Preset.high)
+        XCTAssertEqual(VideoResolution.medium.preset, AVCaptureSession.Preset.medium)
+    }
+
+    func testDescription() {
+        XCTAssertEqual(VideoResolution.photo.description, "photo")
+        XCTAssertEqual(VideoResolution.high.description, "high")
+        XCTAssertEqual(VideoResolution.medium.description, "medium")
+    }
+
+}
